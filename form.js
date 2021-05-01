a35d8871913d6d7457efafbaa0dc3d789e9e57cd
lunoos <65530077+lunoos@users.noreply.github.com>
2021-05-01 23:59:54 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,7 @@
+function show(){
+  document.getElementsByClassName("formSection")[0].style.display = "block";
+}
+
+function hide() {
+  document.getElementsByClassName("formSection")[0].style.display = "none";
+  }
